28bcf2297cc2939c5bdbecd77a309f3ea3f82781
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-03-11 19:36:42 +0900

    feat: タグが重複しないように修正

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,7 +4,7 @@
     <div class="grid grid-cols-2 gap-2 mx-2 pt-10">
       <template v-if="!pending">
         <HashTagCard
-          v-for="tag in res.tags"
+          v-for="tag in tags"
           :key="tag.tag"
           :tag="tag.tag"
           :image="tag.image"
@@ -19,10 +19,24 @@ interface Response {
   tags: {
     tag: string;
     image: string;
-  };
+  }[];
 }
 
-const { data: res, pending } = useFetch<Response>("http://localhost/api/tags");
+const { data: res, pending } = await useFetch<Response>(
+  "http://localhost/api/tags"
+);
+
+const tags = computed(() => {
+  if (!res.value) return [];
+  return res.value.tags.reduce((_tags, _tag) => {
+    if (_tags.some((tagsTag) => tagsTag.tag === _tag.tag)) {
+      return _tags;
+    }
+    return [..._tags, _tag];
+  }, []);
+});
+console.log(tags.value);
+
 </script>
 
 <style scoped lang="sass"></style>
