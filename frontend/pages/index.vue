f14a241b73db56d510a7ead28f4d0bd4a3760296
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-03-11 11:28:31 +0900

    feat: API通信追加

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,26 +2,27 @@
   <div class="bg-base">
     <Header />
     <div class="grid grid-cols-2 gap-2 m-2">
-      <HashTagCard
-        v-for="tag in res.tags"
-        :key="tag.tag"
-        :tag="tag.tag"
-        :image="tag.image"
-      />
+      <template v-if="!pending">
+        <HashTagCard
+          v-for="tag in res.tags"
+          :key="tag.tag"
+          :tag="tag.tag"
+          :image="tag.image"
+        />
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-// TODO: API
-const res = ref({
-  tags: [
-    { tag: "aaa", image: "https://placehold.jp/150x150.png" },
-    { tag: "bbb", image: "https://placehold.jp/150x150.png" },
-    { tag: "ccc", image: "https://placehold.jp/150x150.png" },
-    { tag: "ddd", image: "https://placehold.jp/150x150.png" },
-  ],
-});
+interface Response {
+  tags: {
+    tag: string;
+    image: string;
+  };
+}
+
+const { data: res, pending } = useFetch<Response>("http://localhost/api/tags");
 </script>
 
 <style scoped lang="sass"></style>
