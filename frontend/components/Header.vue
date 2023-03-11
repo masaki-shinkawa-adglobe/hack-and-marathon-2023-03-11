89747ffda720c1e97d72f91101d2b48a2d5900e8
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-03-11 10:55:10 +0900

    feat: header実装

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -66,7 +66,7 @@
               ? 'translate-x-0 opacity-100 '
               : 'opacity-0 -translate-x-full',
           ]"
-          class="absolute inset-x-0 z-20 w-full mt-5 px-6 py-4 transition-all duration-300 ease-in-out bg-white lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center"
+          class="absolute inset-x-0 z-20 w-full mt-4 px-6 py-4 transition-all duration-300 ease-in-out bg-white lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center"
         >
           <div class="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
             <NuxtLink
