2b82f6fdb3878067a3d2d24591833c430c31000b
kotaro.oka <kotaro.oka@adglobe.co.jp>
2023-03-11 13:07:32 +0900

    create backButton component

---
+++ frontend/components/BackButton.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const emit = defineEmits(['handleClick']);
+const handleClick = () => {
+  emit('handleClick');
+};
+</script>
+<template>
+  <div>
+    <button @click="handleClick"><img src="~/assets/img/arrow.svg" class="w-6 h-6"></button>
+  </div>
+</template>
+<style scoped></style>
