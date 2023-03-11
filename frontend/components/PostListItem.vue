a57ab303373fed57b92367645f1dafbc35e89643
KazuhikoNakano <kazuhiko.nakano@adglobe.co.jp>
2023-03-11 12:31:25 +0900

    WIP：投稿一覧（スタイル未適用）

---
+++ frontend/components/PostListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLink
+    :href="`/posts/${props.id}`"
+    role="link"
+    class="w-full bg-base"
+  >
+    <img
+      class="w-full"
+      :src="props.image"
+      alt="postImage" />
+    <div>
+      {{props.title}}
+    </div>
+    <div>
+      {{props.detail}}
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  id: number;
+  title: string;
+  detail: string;
+  image: string;
+  isBookmark: boolean;
+  date: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
