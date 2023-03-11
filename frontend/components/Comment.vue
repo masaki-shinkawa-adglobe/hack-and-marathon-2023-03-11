b86555126c92180901ac4658b122fd839f053ece
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-03-11 12:46:53 +0900

    feat: コメント一覧実装

---
+++ frontend/components/Comment.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="flex justify-between text-main">
+      <span class="whitespace-nowrap overflow-hidden text-ellipsis flex-1 font-bold">{{
+        name
+      }}</span>
+      <span>{{ date }}</span>
+    </div>
+    <p class="border-b-2 text-main pb-2">{{ comment }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { format } from "date-fns";
+interface Comment {
+  comment: string;
+  name: string;
+  date: string;
+}
+
+const props = defineProps<Comment>();
+
+const date = computed(() => {
+  return format(new Date(props.date), "yyyy/MM/dd HH:mm:ss");
+});
+</script>
+
+<style scoped lang="sass"></style>
--- frontend/pages/posts/[id]/comments.vue
+++ frontend/pages/posts/[id]/comments.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>posts/id/comments</div>
+  <div class="h-screen bg-base">
+    <Header />
+    <div class="m-2">
+      <Comment
+        v-if="data"
+        v-for="comment in data.comments"
+        :name="comment.name"
+        :comment="comment.comment"
+        :date="comment.date"
+      />
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface CommentsResponse {
+  comments: [
+    {
+      comment: string;
+      name: string;
+      date: string;
+    }
+  ];
+}
+
+const { params } = useRoute();
+const { data } = await useFetch<CommentsResponse>(
+  `http://localhost/api/posts/${params.id}/comment`
+);
+</script>
 
 <style scoped lang="sass"></style>
