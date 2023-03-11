65604790de57ce8b2a8f2fde4af02877fb50bc4c
takayuki-miyazaki-adglobe-co-jp <takayuki.miyazaki@adglobe.co.jp>
2023-03-11 11:10:26 +0900

    feat: ルーティングのガワ作成

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 投稿一覧
+Route::get('/posts', function (Request $request) {
+    return "";
+});
+
+// 投稿
+Route::post('/posts', function (Request $request) {
+    return "";
+});
+
+// 投稿詳細
+Route::get('/posts/{postId}', function (Request $request) {
+    return "";
+});
+
+// ブックマーク切り替え
+Route::post('/posts/{postId}/bookmark', function (Request $request) {
+    return "";
+});
+
+// いいね切り替え
+Route::post('/images/{imageId}/like', function (Request $request) {
+    return "";
+});
+
+// ハッシュタグ一覧
+Route::get('/tags', function (Request $request) {
+    return "";
+});
+
+// コメント一覧
+Route::get('/posts/{postId}/comment', function (Request $request) {
+    return "";
+});
+
+// コメント投稿
+Route::post('/posts/{postId}/comment', function (Request $request) {
+    return "";
+});
