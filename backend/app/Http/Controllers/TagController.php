9b77fda1699db1e105a6e4aaeb744d3d63e16a50
takayuki-miyazaki-adglobe-co-jp <takayuki.miyazaki@adglobe.co.jp>
2023-03-11 19:16:10 +0900

    fix: レスポンス修正

--- backend/app/Http/Controllers/TagController.php
+++ backend/app/Http/Controllers/TagController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\PostTag;
+use App\Models\PostImage;
+use Illuminate\Support\Facades\DB;
 
 class TagController extends Controller
 {
@@ -13,10 +16,19 @@ class TagController extends Controller
      */
     public function index(): array
     {
-        $tags = new PostTag();
+        $subQuery = PostImage::select("post_images.post_id", "post_images.base64", DB::raw("count(post_image_likes.id) as like_count"))
+            ->leftJoin("post_image_likes", "post_images.id", "=", "post_image_likes.post_image_id")
+            ->groupBy("post_images.post_id", "post_images.base64");
+        $rows = PostTag::select("tag", "sub.base64", "sub.like_count")
+            ->leftJoinSub($subQuery, "sub", fn ($join) => $join->on("post_tags.post_id", "=", "sub.post_id"))
+            ->orderByDesc("like_count")
+            ->get();
 
-        $response = $tags->getTags();
-
-        return ['tags' => $response];
+        return [
+            "tags" => $rows->map(fn ($item) => [
+                "tag" => $item->tag,
+                "image" => $item->base64,
+            ])
+        ];
     }
 }
